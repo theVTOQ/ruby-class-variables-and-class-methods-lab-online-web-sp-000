52f3d5e35fd7e3b5ba68c1abf37a96e2183cb538
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-15 15:33:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,36 @@
 class Song
   attr_accessor :name, :artist, :genre
-  ARTISTS = []
-  GENRES = []
+  @@artists = {}
+  @@genres = {}
   @@count = 0
 
   def initialize(artist, genre)
     @artist = artist
     @genre = genre
-    ARTISTS << artist
-    GENRES << genre
+
+    add_artist(artist)
+    add_genre(genre)
+    
     @@count += 1
   end
+
+  def add_artist(artist)
+    if @@artists[artist] == nil 
+      @@artists[artist] = 1
+    else
+      @@artists[artist] += 1
+    end
+  end
+
+  def add_genre
+    if @@genres[genre] == nil 
+      @@genres[genre] = 1
+    else
+      @@genres[genre] += 1
+    end
+  end
+  
+  def self.genre_count(genre)
+    @@genres[genre]
+  end
+end
